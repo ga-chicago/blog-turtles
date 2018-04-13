099428c4758d8e1f6842e0c6d69fbf64a80dc4f5
Jim Haff <Jimmyhaff3@gmail.com>
2018-04-13 11:47:22 -0500

    added author model

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const Authors = require('../models/authors');
 
 router.get('/', (req, res) => {
-  res.render('authors/index.ejs');
+
+  Authors.find((err, foundAuthors) => {
+
+    if(err) console.log(err);
+    console.log(foundAuthors, ' this is found Authors')
+
+    res.render('authors/index.ejs', {authors: foundAuthors});
+  });
 });
 
 
