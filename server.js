7e6199dcd0c8c5b5ae349450cd74e523159f0468
Reuben Ayres <ra@sent.com>
2018-04-13 16:02:07 -0500

    article controller.  article index route.  article index view template

--- server.js
+++ server.js
@@ -6,15 +6,16 @@ const methodOverride = require('method-override');
 
 // run our db file
 require('./db/db');
-const authorsController = require('./controllers/authors');
 
 // MIDDLEWARE
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 // CONTROLLERS
+const authorsController = require('./controllers/authors');
 app.use('/authors', authorsController);
-
+const articlesController = require('./controllers/articles')
+app.use('/articles', articlesController);
 
 
 app.listen(3000, () => {
