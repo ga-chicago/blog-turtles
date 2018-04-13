eeae28579f1b2d9a0f7a6331d04d5b948d10cd48
Reuben Ayres <ra@sent.com>
2018-04-13 16:56:20 -0500

    added index, new, show, and create for articles

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Articles = require('../models/article.js')
 
 router.get('/', (req, res) => {
-	res.render('articles/index.ejs')
+	Articles.find((err, theArticlesIFound) => {
+		res.render('articles/index.ejs', {
+			articles: theArticlesIFound
+		})	
+	})
+})
+
+router.get('/new', (req, res) => {
+	res.render('articles/new.ejs')
+})
+
+router.get('/:id', (req, res) => {
+	Articles.findById(req.params.id, (err, thisArticle) => {
+		res.render('articles/show.ejs', {
+			article: thisArticle
+		})
+	})	
+})
+
+
+router.post('/', (req, res) => {
+	Articles.create(req.body, (err, createdArticle) => {
+		res.redirect('/articles')
+	})
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -17,6 +17,24 @@ app.use('/authors', authorsController);
 const articlesController = require('./controllers/articles')
 app.use('/articles', articlesController);
 
+// seeding a database is adding some data to the database so there some data there to work with when you are doing develoipment
+app.get('/seed', (req, res) => {
+	// Articles.create
+	// Articles.create
+	// Articles.create
+	// Articles.create
+	// Articles.create
+
+	// Authors.create
+	// Authors.create
+	// Authors.create
+	// Authors.create
+	// Authors.create
+
+	res.send("I just added some data for you")
+
+
+})
 
 app.listen(3000, () => {
   console.log('server is listening on PORT: 3000')
